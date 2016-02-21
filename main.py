02fc0e35965825f984312013c663c10630a5ae93
Zach Munro-Cape <zach.munrocape@gmail.com>
2016-02-21 14:01:02 -0500

    added gitignore for pickle

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
-import requests
 import bs4
+import cPickle as pickle
+import requests
 
 BOTTLESHOP = 'http://www.bellwoodsbrewery.com/product-category/bottleshop/'
 
+PICKLE_FILE = 'beerlist.pickle'
+
 def get_current_beers():
     ''' '''
+
     # request the page
     res = requests.get(BOTTLESHOP)
     # grab the ul that has the products
     soup = bs4.BeautifulSoup(res.text, "html.parser")
-
-    # a ul has li elements - one for each beer
+    # the ul#products has a li for each beer
     beers = soup.findAll("ul", { "class" : "products" })[0].findAll('li')
+
+    parsed_beers = []
 
     for beer in beers:
     	title = beer.find('h3').getText()
     	link = beer.find('a')['href']
-    	print title, link
+    	parsed_beers.append({'title': title, 'link': link})
 
-    return
+    return parsed_beers
+
+def get_beerlist():
+	try:
+		p = open(PICKLE_FILE, 'r')
+		return pickle.load(p)
+	except IOError:
+		return {}
+
+def save_beerlist():
+	p = open(PICKLE_FILE, 'wb')
+	pickle.dump(p)
+	return
+
+def proc():
+
+	# get the beers
+
+	# process the beers
+
+	return
 
 if __name__ == '__main__':
-    get_current_beers()
+    proc()
