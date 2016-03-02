62a00b8afe1f7e2b4947b3674aeec27157ec0a6a
Zach Munro-Cape <zach.munrocape@gmail.com>
2016-03-02 11:40:49 -0500

    Weird encoding issue on DO fixed

--- breweries/rainhard.py
+++ breweries/rainhard.py
@@ -6,10 +6,8 @@ class Rainhard(Brewery):
 		parsed_beers = []
 		beers = soup.findAll("div", { "data-q_id" : "#portfolio" })
 		for b in beers:
-			#print b
 			title = b.find('strong').getText().split('(')[0]
 			if 'COMING SOON' not in title:
-				print title
 				link = self.bottleshop_url
 				parsed_beers.append({'title': title, 'link': link})
 		return parsed_beers
