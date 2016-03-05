bbdb8d35d66dfeedaed9ed4cc348acb43f34d812
Zach Munro-Cape <zach.munrocape@gmail.com>
2016-03-05 16:33:03 -0500

    Added loading message

--- src/js/bottleshopController.js
+++ src/js/bottleshopController.js
@@ -4,11 +4,13 @@ bottleshopApp.controller('BottleshopController', ['$scope', '$http', '$compile',
 	$scope.listings = [];
 	$scope.recent = [];
 	$scope.refreshing = true;
-    $scope.headerMessage = 'Fetching the list - coming in hot off the presses ...';
+    $scope.beerRange = [];
+    $scope.headerMessage = 'Fetching the list - coming in hot off the presses . . .';
     $scope.init = function () {
-    	$http.get('/api/listings').then(
-    		function success(rep) {
-    			$scope.listings = rep.data['beers'];
+        beerInterval = setInterval($scope.addEmoji, 550);
+        $http.get('/api/listings').then(
+            function success(rep) {
+                $scope.listings = rep.data['beers'];
                 var i;
                 for(i = 0; i < $scope.listings.length; i++){
                     recent = $scope.listings[i].beers.filter($scope.recentBeerFilter);
@@ -16,14 +18,22 @@ bottleshopApp.controller('BottleshopController', ['$scope', '$http', '$compile',
                         $scope.recent.push({'brewery': $scope.listings[i].brewery, 'beers': recent});
                     }
                 }
-    			$scope.refreshing = false;
-                console.log($scope.recent);
-    		}, 
-    		function error(rep) {
+                $scope.refreshing = false;
+                clearInterval(beerInterval);
+            }, 
+            function error(rep) {
                 $scope.headerMessage = 'Beer listings are unavailable :(';
-    		}
-    	);
+                $scope.beerRange = [];
+                $scope.$apply();
+            }
+        );
     };
+
+    $scope.addEmoji = function () {
+        $scope.beerRange.push($scope.beerRange.length);
+        // need to use $apply to get angular to update DOM
+        $scope.$apply();
+    }
 
     $scope.recentBeerFilter = function (beer) {
         return beer.recently_added;
